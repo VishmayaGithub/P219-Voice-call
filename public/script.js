91235f9f288500340cd95ea2bf919259113c0b96
VishmayaGithub <72074398+VishmayaGithub@users.noreply.github.com>
2023-06-10 11:42:56 +0530

    Second commit

--- public/script.js
+++ public/script.js
@@ -22,7 +22,14 @@ navigator.mediaDevices
         socket.on("user-connected",(userId)=>{
             connectToNewUser(userId,stream)
         })
-        
+        peer.on("call",(call)=>{
+            call.answer(stream)
+            const video =document.createElement("video")
+            call.on("stream",(userVideoStream)=>{
+                addVideoStream(video,userVideoStream)
+            })
+        })
+
     })
 
 function addVideoStream(video, stream) {
